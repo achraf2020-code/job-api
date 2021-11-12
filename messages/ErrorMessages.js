ce863dbde63772f22e2b7e30a22b047ee3cb0d4d
achraf2020-code <achraf-saidi@outlook.com>
2021-11-12 21:30:00 +0100

    add login controller

--- messages/ErrorMessages.js
+++ messages/ErrorMessages.js
@@ -2,6 +2,7 @@ const errorMessages = {
     NotFoundError:"Not Found!" ,
     BadRequest:'Please Check Your Entry!',
     UnAuthorized:'UnAuthorizid Access!',
-    InternalServerError:'Somthing Went Wrong Please Try Leater!'
+    InternalServerError:'Somthing Went Wrong Please Try Leater!',
+    PasswordNotValid:'Please check your password!'
 }
 module.exports = errorMessages
