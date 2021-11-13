3cdd84ef387e3114dfbc2756312a57453ae7c341
achraf2020-code <achraf-saidi@outlook.com>
2021-11-13 01:44:46 +0100

    add security pakages

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const dotenv =  require('dotenv')
+//security pakages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
 
 
 //app required files
@@ -12,8 +17,17 @@ const errorHandler = require('./middlewars/errorHandler')
 //app configure
 const app = express()
 dotenv.config()
+
 const port = process.env.PORT || 3000 
 //app middleware
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.use(rateLimit({
+    windowMs:15 * 60 * 1000,
+    limit:100
+}))
+
 app.use(express.json())
 app.use('/api/v1',appRoutes)
 app.use(notFound)
